7b71b4a1f0154e9169a5f1937bc6423d170953cb
ZweiZhao <ZweiZhao@foxmail.com>
2017-02-24 16:16:58 +0800

    新增解析普通链接参数与去除普通英文括号

--- utils.js
+++ utils.js
@@ -32,4 +32,29 @@ export default class Utils {
 				callback(JSON.parse(responseText));
 			}).done();
 	}
+	
+	//去除英文括号
+	static clearBracket(aString) {
+		if(aString.indexOf("(") >= 0) {
+			aString = aString.replace("(", "")
+			aString = aString.replace(")", "")
+		}
+		return aString
+	}
+
+	//解析普通链接的参数
+	static analysisUriQuery(uri) {
+		var urlSearch = uri.search
+		var params = new Object()
+		if(urlSearch.indexOf("?") == 0) {
+			var paramsString = urlSearch.substr(1)
+			var paramLink = paramsString.split("&linkUrl=")
+			params.linkUrl = paramLink[1]
+			var paramsStrings = paramLink[0].split("&")
+			for(var i = 0; i < paramsStrings.length; i++) {
+				params[paramsStrings[i].split("=")[0]] = paramsStrings[i].split("=")[1];
+			}
+		}
+		return params
+	}
 }
